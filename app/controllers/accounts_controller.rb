8cf7b3d1df293bb4fba95c845f67a18235d6db69
kanji <kanjihtmt@gmail.com>
2015-10-19 20:09:15 +0900

    マイページの雛形を作成

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,19 @@
+class AccountsController < ApplicationController
+  def my
+  end
+
+  def show
+  end
+
+  def photo
+  end
+
+  def password
+  end
+
+  def email
+  end
+
+  def delete
+  end
+end
