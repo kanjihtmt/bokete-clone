fe7e09ff033c4d5c5e5f5dd6a4cdcc20cd4a5b85
kanji <kanjihtmt@gmail.com>
2015-10-20 11:01:42 +0000

    新規登録画面のレイアウトを調整

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20151019100940) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "bokes", force: :cascade do |t|
     t.text     "text"
     t.string   "tag"
