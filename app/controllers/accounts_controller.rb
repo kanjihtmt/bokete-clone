4b64d9529c2a05a7f887adf2c876e7df620fd5df
kanji <kanjihtmt@gmail.com>
2015-10-22 08:27:56 +0000

    パスワード変更、メールアドレス変更のレイアウトを調整

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,9 +1,10 @@
 class AccountsController < ApplicationController
+  before_action :set_user, only: %i(edit photo password email)
+
   def show
   end
 
   def edit
-    @user = current_user
   end
 
   def update
@@ -18,7 +19,22 @@ class AccountsController < ApplicationController
   def password
   end
 
+  def update_password
+  end
+
   def email
   end
 
+  private
+    def set_user
+      @user = current_user
+    end
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirm)
+    end
+
+    def password_params
+      params.require(:user).permit(:current_password, :password, :password_confirm)
+    end
 end
