caab00cd336cbc4a1b269d7ef9511bfc8521927e
kanji <kanjihtmt@gmail.com>
2015-10-26 05:16:37 +0000

    ボケ投稿の画面を完成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create(name: 'バカ')
+Category.create(name: 'シュール ')
+Category.create(name: 'お下劣')
+Category.create(name: 'ブラック')
+Category.create(name: '身内')
+Category.create(name: '例え')
+Category.create(name: 'その他')
+
+
