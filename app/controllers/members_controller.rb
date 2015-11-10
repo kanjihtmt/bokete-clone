7e7f0e8a648560afd48ebf6908a9440a9fcac11b
kanji <kanjihtmt@gmail.com>
2015-11-11 06:53:09 +0900

    ユーザページのトップ画面を動的に表示

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -2,12 +2,15 @@ class MembersController < ApplicationController
   before_action :set_user
 
   def show
+    @bokes = @user.bokes.order(created_at: :desc).limit(3)
   end
 
   def bokes
+    @bokes = @user.bokes.page(params[:page]).order(created_at: :desc)
   end
 
   def themes
+    @themes = @user.themes.page(params[:page]).order(created_at: :desc)
   end
 
   def favorites
