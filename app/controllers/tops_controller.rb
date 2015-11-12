25f033bd6d34a319733027c3dcc6cde70aae9a1f
kanji <kanjihtmt@gmail.com>
2015-11-13 07:23:32 +0900

    トップページの人気ボケに実データを表示

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,4 +1,7 @@
 class TopsController < ApplicationController
   def index
+    @popular = Boke.find(Boke.sort(:popular).limit(50).pluck(:id).sample)
+    @hots = Boke.sort(:hot).limit(3)
+    @recents = Boke.sort(:recent).limit(6)
   end
 end
