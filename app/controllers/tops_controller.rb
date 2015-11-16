b8af53617c263920e622288b0988f5e0f6d4fc9e
kanji <kanjihtmt@gmail.com>
2015-11-16 20:59:01 +0900

    トップページを実装

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,6 +1,6 @@
 class TopsController < ApplicationController
   def index
-    @popular = Boke.find(Boke.sort(:popular).limit(50).pluck(:id).sample)
+    @popular = Boke.find(Boke.sort(:popular).limit(50).pluck(:id).sample || Boke.all.pluck(:id).sample)
     @hots = Boke.sort(:hot).limit(3)
     @recents = Boke.sort(:recent).limit(6)
   end
