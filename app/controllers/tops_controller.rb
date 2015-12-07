e1e798202fc31ffac6f89abb6bca406b863df4fd
kanji <kanjihtmt@gmail.com>
2015-12-07 08:54:20 +0000

    Bugfix: データが1件も無い時、トップページの人気ボケで落ちるバグを修正

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,7 +1,14 @@
 class TopsController < ApplicationController
+  HOT_MAX = 3
+  RECENT_MAX = 6
+
   def index
-    @popular = Boke.find(Boke.sort(:popular).limit(50).pluck(:id).sample || Boke.all.pluck(:id).sample)
-    @hots = Boke.sort(:hot).limit(3)
-    @recents = Boke.sort(:recent).limit(6)
+    if Boke.sort(:popular).count > 0 || Boke.count > 0
+      @popular = Boke.find(Boke.sort(:popular).pluck(:id).sample || Boke.all.pluck(:id).sample)
+    else
+      @popular = nil
+    end
+    @hots = Boke.sort(:hot).limit(HOT_MAX)
+    @recents = Boke.sort(:recent).limit(RECENT_MAX)
   end
 end
